63c4da2706885993564da0f14411e1d7521a7a06
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2024-03-09 03:37:53 -0400

    fixed gradle

--- medsys-api/src/test/java/com/bo/medsys/automation/medsys/api/runner/RunnerTest.java
+++ medsys-api/src/test/java/com/bo/medsys/automation/medsys/api/runner/RunnerTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = {"src/test/resources/features/"},
+        features = {"medsys-api/src/test/resources/features/"},
         glue = {"com.bo.medsys.automation.medsys.api"},
         plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
